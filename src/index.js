604c6fe60e041bc8dff02acf31562f5ede1890f1
mg3442 <magnusprotas@gmail.com>
2018-09-23 23:00:58 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency>10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if(currency==0) return {};
+
+    var res={};
+    var sum=currency;
+
+    if(currency>0 || currency <=10000){
+       if(sum>=50){
+           res.H = parseInt(sum/50);
+           sum=sum-res.H * 50;
+
+       }
+       if(sum>=25){
+           res.Q = parseInt(sum/25);
+           sum=sum-res.Q*25;
+
+       }
+       if(sum>=10){
+           res.D = parseInt(sum/10);
+           sum = sum-res.D*10;
+
+       }
+       if(sum>=5){
+           res.N = parseInt(sum/5);
+           sum = sum-res.N*5;
+
+       }
+       if(sum>=1){
+           res.P = parseInt(sum);
+       }
+       return res;
+    }
 }
